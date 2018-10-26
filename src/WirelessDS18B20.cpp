3570bbefd009680b58374cc52b0907352447bb30
J6B <J6B@hotmail.fr>
2018-10-26 21:17:04 +0200

    Switch to Base V1.1.5

--- src/WirelessDS18B20.cpp
+++ src/WirelessDS18B20.cpp
@@ -856,6 +856,43 @@ bool WebDS18B20Buses::AppInit(bool reInit)
   return true;
 };
 //------------------------------------------
+//Return HTML Code to insert into Status Web page
+const uint8_t* WebDS18B20Buses::GetHTMLContent(WebPageForPlaceHolder wp){
+      switch(wp){
+    case status:
+      return (const uint8_t*) status1htmlgz;
+      break;
+    case config:
+      return (const uint8_t*) config1htmlgz;
+      break;
+    case fw:
+      return (const uint8_t*) fw1htmlgz;
+      break;
+    case discover:
+      return (const uint8_t*) discover1htmlgz;
+      break;
+  };
+  return nullptr;
+};
+//and his Size
+size_t WebDS18B20Buses::GetHTMLContentSize(WebPageForPlaceHolder wp){
+  switch(wp){
+    case status:
+      return sizeof(status1htmlgz);
+      break;
+    case config:
+      return sizeof(config1htmlgz);
+      break;
+    case fw:
+      return sizeof(fw1htmlgz);
+      break;
+    case discover:
+      return sizeof(discover1htmlgz);
+      break;
+  };
+  return 0;
+};
+//------------------------------------------
 //code to register web request answer to the web server
 void WebDS18B20Buses::AppInitWebServer(AsyncWebServer &server, bool &shouldReboot, bool &pauseApplication)
 {
